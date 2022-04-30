ae449497372faaafddcb188a92a1d566a5ae7ffe
amitkrt-pe <amit.thakur@pharmeasy.in>
2022-04-30 16:08:16 +0530

    whatsapp

--- apps/home.py
+++ apps/home.py
@@ -10,10 +10,9 @@ def app():
     #Current Dues
     duesDf = am.read_gsheet("1btdfIIxZYTHpadDRxkKDEhOzh8NnFEUB5ugrWPOMgTs","Sheet8")
     finaldf = duesDf.dropna()
-    finaldf.rename(columns = {'flatNo':'Flat No','tenantName':'Tenant Name','paymentDue':'Current Dues'}, inplace = True)
-    displayDf = finaldf[finaldf['Current Dues']>0][['Flat No','Tenant Name','Current Dues']]
-    displayDf.sort_values(by=['Current Dues'], ascending=False, inplace=True)
-    totalDue = displayDf['Current Dues'].sum()
+    displayDf = finaldf[finaldf['paymentDue']>0][['flatNo','tenantName','paymentDue']]
+    displayDf.sort_values(by=['paymentDue'], ascending=False, inplace=True)
+    totalDue = displayDf['paymentDue'].sum()
     lendf = len(displayDf)
     st.markdown(f"<h3 style='text-align: center;'>Current Dues = {totalDue}</h3>", unsafe_allow_html=True)
 
@@ -31,7 +30,7 @@ def app():
         height=40
     ),
     cells=dict(
-        values=[displayDf['Flat No'],displayDf['Tenant Name'],displayDf['Current Dues']],
+        values=[displayDf['flatNo'],displayDf['tenantName'],displayDf['paymentDue']],
         line_color='darkslategray',
         fill=dict(color=['paleturquoise', 'white']),
         align=['center', 'left', 'right'],
@@ -41,16 +40,4 @@ def app():
     ])
     fig.update_layout(width=370, height=(50+((lendf+1)*30)), margin=dict(l=0, r=0, t=0, b=0))
     st.write(fig)
-
-    #Vacant Flats
-    flatsDf = am.read_gsheet("1btdfIIxZYTHpadDRxkKDEhOzh8NnFEUB5ugrWPOMgTs","Sheet10")
-    occupiedFlatList = list(finaldf['Flat No'])
-    flatList = list(flatsDf['flatNo'])
-    vacantFlatList = []
-    for x in flatList:
-        if x not in occupiedFlatList:
-            vacantFlatList.append(x)
-    vacantFlatDf = flatsDf[flatsDf['flatNo'].isin(vacantFlatList)]
-    vacantFlatDf.rename(columns={'flatNo':'Flat No.','type':'Type'}, inplace = True)
-    st.markdown(f"<h3 style='text-align: center;'>Vaccant Flats = {len(vacantFlatDf)}</h3>", unsafe_allow_html=True)
-    st.table(vacantFlatDf)
+    pywhatkit.sendwhatmsg("+918837245956","Geeks For Geeks!",16,15,True,5)

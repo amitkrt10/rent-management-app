97e97d66070f56ba2aef78b24b511d8cd2eb8159
amitkrt-pe <amit.thakur@pharmeasy.in>
2022-05-01 09:25:25 +0530

    meter reading added

--- apps/home.py
+++ apps/home.py
@@ -1,4 +1,6 @@
 from email import message
+from operator import index
+from xml.dom.pulldom import IGNORABLE_WHITESPACE
 import streamlit as st
 def app():
     import pandas as pd
@@ -32,6 +34,46 @@ def app():
     ),
     cells=dict(
         values=[displayDf['flatNo'],displayDf['tenantName'],displayDf['paymentDue']],
+        line_color='darkslategray',
+        fill=dict(color=['paleturquoise', 'white']),
+        align=['center', 'left', 'right'],
+        font=dict(color='black', size=14),
+        height=30)
+        )
+    ])
+    fig.update_layout(width=370, height=(50+((lendf+1)*30)), margin=dict(l=0, r=0, t=0, b=0))
+    st.write(fig)
+
+    #Electrictity Usage
+    meterDf = am.read_gsheet("1btdfIIxZYTHpadDRxkKDEhOzh8NnFEUB5ugrWPOMgTs","Sheet5")
+    meterDf = meterDf[meterDf.columns.drop(list(meterDf.filter(regex='Unnamed')))]
+    meterDf.drop(['readingDate'],axis=1,inplace=True)
+    maxIndex = meterDf.index.max()
+    meterDisplayDf = meterDf[meterDf.index>=maxIndex-1]
+    meterDisplayDf.set_index(['readingMonth'],inplace=True)
+    meterDisplayDf = meterDisplayDf.T
+    meterDfCols = list(meterDisplayDf.columns)
+    meterDisplayDf["Units"] = meterDisplayDf[meterDfCols[1]] - meterDisplayDf[meterDfCols[0]]
+    meterDisplayDf['Flat No.'] = meterDisplayDf.index
+    totalUsage = meterDisplayDf['Units'].sum()
+    st.markdown(f"<h3 style='text-align: center;'>Electricity Usage  = {totalUsage} KW</h3>", unsafe_allow_html=True)
+
+    fig = go.Figure(data=[go.Table(
+    columnorder = [1,2,3,4],
+    columnwidth = [40,50,50,40],
+    header = dict(
+        values = [['<b>Flat No.</b>'],
+                    [f'<b>{meterDfCols[0]}</b>'],
+                    [f'<b>{meterDfCols[1]}</b>'],
+                    [f'<b>Units</b>']],
+        line_color='darkslategray',
+        fill_color='royalblue',
+        align='center',
+        font=dict(color='white', size=16),
+        height=40
+    ),
+    cells=dict(
+        values=[meterDisplayDf['Flat No.'],meterDisplayDf[meterDfCols[1]],meterDisplayDf[meterDfCols[0]],meterDisplayDf['Units']],
         line_color='darkslategray',
         fill=dict(color=['paleturquoise', 'white']),
         align=['center', 'left', 'right'],

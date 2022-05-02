18b8760e82b873b906bf7f3d4497363e8a1037e9
amitkrt-pe <amit.thakur@pharmeasy.in>
2022-05-02 10:15:32 +0530

    added bank statement filters

--- apps/onlineTransfers.py
+++ apps/onlineTransfers.py
@@ -1,3 +1,4 @@
+from email import header
 from operator import le
 from threading import stack_size
 import streamlit as st
@@ -57,3 +58,50 @@ def app():
     fig.update_layout(width=370, height=(100+((lendf+1)*30)), margin=dict(l=0, r=0, t=50, b=0))
     st.write(fig)
 
+    st.markdown("<h3 style='text-align: center;'>Filters</h3>", unsafe_allow_html=True)
+    dfFilter = st.selectbox("Select Category",['Select','Rent','Travel','Bills'])
+    if dfFilter=='Select':
+        st.write('Select a category')
+    if dfFilter=='Rent':
+        statementDf = bankDf[bankDf['Remark'].str.contains('Rent')][['Date','Deposit','Remark']]
+        headerCol = 'Income'
+    if dfFilter=='Travel':
+        statementDf = bankDf[bankDf['Remark'].str.contains('Train')][['Date','Withdrawal','Remark']]
+        headerCol = 'Expense'
+    if dfFilter=='Bills':
+        statementDf = bankDf[bankDf['Remark'].str.contains('Bill')][['Date','Withdrawal','Remark']]
+        headerCol = 'Expense'
+
+    if dfFilter!='Select':
+        lendf = len(statementDf)+1
+        viewCols = list(statementDf.columns)
+        dfValues = [list(statementDf[viewCols[0]]) + ['<b>Total</b>'],
+                        list(statementDf[viewCols[1]]) + [f'<b>{statementDf[viewCols[1]].sum()}</b>'],
+                        list(statementDf[viewCols[2]]) + ['<b>---</b>']]
+
+        fig = go.Figure(data=[go.Table(
+        columnorder = [1,2,3],
+        columnwidth = [40,40,100],
+        header = dict(
+            values = [['<b>Date</b>'],
+                        [f'<b>{headerCol}</b>'],
+                        ['<b>Remark</b>']],
+            line_color='darkslategray',
+            fill_color='royalblue',
+            align='center',
+            font=dict(color='white', size=16),
+            height=40
+        ),
+        cells=dict(
+            values=dfValues,
+            line_color='darkslategray',
+            fill=dict(color=['paleturquoise', 'white', 'white']),
+            align=['right', 'right', 'left'],
+            font=dict(color='black', size=14),
+            height=30)
+            )
+        ]
+        #layout=go.Layout(title=go.layout.Title(text=f"<b>{tenantName}</b> | Flat No. : <b>{flatNo}</b>"))
+        )
+        fig.update_layout(width=370, height=(100+((lendf+1)*30)), margin=dict(l=0, r=0, t=50, b=0))
+        st.write(fig)
